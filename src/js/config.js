63994866087683407254d048f3eb8c45cfc02974
aniket30i <iamaniket.saha.09@protonmail.com>
2024-01-30 20:22:20 +0530

    finished

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-
 export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1907a44e-0604-4cec-840d-9642aa2ef383';
+export const MODAL_CLOSE_SEC = 2.5;
